9224ed81fec061b9a43fb42a4fe9d5e65dae85fb
Fadhil <fadhilahdwi.a@gmail.com>
2024-06-17 12:00:30 +0700

    feat: change metadata

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>FE Test</title>
+        <meta name="description" content="FE Test" />
+        <meta name="keywords" content="FE Test" />
+      </Head>
       <body>
         <Main />
         <NextScript />
